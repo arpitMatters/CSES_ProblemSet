6c70e8a24950ff3dfc642e6dcf48d5933373a03d
arpitMatters <43750177+arpitMatters@users.noreply.github.com>
2021-07-10 18:27:03 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -18,9 +18,9 @@ int main() {
     for (int i = 1; i < n; i += 2)
       cout << i << " ";
   } else {
-    for (int i = n; i > 0; i -= 2)
+    for (int i = 1; i <= n; i += 2)
       cout << i << " ";
-    for (int i = n - 1; i > 0; i -= 2)
+    for (int i = 2; i < n; i += 2)
       cout << i << " ";
   }
   return 0;
